ed67d096eded94f8f24012d4b5d67a2f428aa78b
jianghang <hangscer@hotmail.com>
2018-10-21 15:37:01 +0800

    优化系统托盘

--- src/main/scala/win/hangscer/webQQLinux/Main.scala
+++ src/main/scala/win/hangscer/webQQLinux/Main.scala
@@ -1,6 +1,7 @@
 package win.hangscer.webQQLinux
 
 import java.awt._
+import java.awt.event._
 import java.nio.file.Paths
 
 import com.teamdev.jxbrowser.chromium._
@@ -13,9 +14,11 @@ import scala.util.Try
 object QQWeb extends App {
   val userDataPath = Paths.get(System.getProperty("user.home") + "/.webQQLinux" + "/Cache")
   val lazyFileOption = () => Option(userDataPath.toFile).flatMap(f => Option(f, f.exists())) match {
-    case Some((f, false)) => f.mkdirs()
+    case Some((f, false)) =>
+      f.mkdirs()
     case _ =>
   }
+
   Try(lazyFileOption())
   BrowerUtil.firstStep()
   val params = new BrowserContextParams(userDataPath.toString)
@@ -24,16 +27,38 @@ object QQWeb extends App {
   val browser = new Browser(BrowserType.LIGHTWEIGHT, context)
   val view = new BrowserView(browser)
   val jFrame = new JFrame("webQQLinux")
-  jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
   jFrame.add(view, BorderLayout.CENTER)
   jFrame.setSize(420, 650)
   jFrame.setVisible(true)
+  jFrame.addWindowListener(new WindowAdapter {
+    override def windowClosing(e: WindowEvent): Unit = jFrame.setVisible(false)
+  })
   browser.loadURL("https://web2.qq.com")
+
+  Option(System.getProperty("os.name")).map(p => p.contains("mac") || p.contains("Mac")) match {
+    case Some(true) =>
+      Try(com.apple.eawt.Application.getApplication.setDockIconImage(new ImageIcon(this.getClass.getResource("/img/qq3.png")).getImage))
+    case _ =>
+      jFrame.setIconImage(new ImageIcon(this.getClass.getResource("/img/qq3.png")).getImage)
+  }
 
   val icon = new ImageIcon(this.getClass.getResource("/img/qq.png"))
   val trayIcon = new TrayIcon(icon.getImage)
   trayIcon.setToolTip("webQQLinux")
+  trayIcon.setImageAutoSize(true)
   trayIcon.setPopupMenu(Option(new PopupMenu())
+    .map { menu =>
+      val menuItem = new MenuItem("show")
+      menuItem.addActionListener { _ =>
+        jFrame.setVisible(true)
+      }
+      menu.add(menuItem)
+      menu
+    }
+    .map { menu =>
+      menu.addSeparator()
+      menu
+    }
     .map { menu =>
       val menuItem = new MenuItem("refresh")
       menuItem.addActionListener { _ =>
