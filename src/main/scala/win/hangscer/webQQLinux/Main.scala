2c8165eebd7c3e7da01ae6cdd4a1be1c0e86e5ed
hangscer <hangscer@hotmail.com>
2018-10-23 00:18:48 +0800

    other

--- src/main/scala/win/hangscer/webQQLinux/Main.scala
+++ src/main/scala/win/hangscer/webQQLinux/Main.scala
@@ -4,9 +4,11 @@ import javafx.application.Application
 import javafx.scene.web.WebView
 import javafx.scene.{Group, Scene}
 import javafx.stage.Stage
+import javax.net.ssl.HttpsURLConnection
 
 class QQWebMainApplication extends Application {
   override def start(stage: Stage): Unit = {
+    HttpsURLConnection.setDefaultHostnameVerifier((_, _) => true)
     stage.setResizable(false)
     stage.setWidth(400)
     stage.setHeight(650)
@@ -15,6 +17,7 @@ class QQWebMainApplication extends Application {
     val webEngine = webView.getEngine
     scene.setRoot(webView)
     stage.setScene(scene)
+    webView.setContextMenuEnabled(false) //右键　网页　菜单　禁止
     webEngine.load("https://web2.qq.com/")
     stage.show()
   }
